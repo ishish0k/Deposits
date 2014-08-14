4f9ceaf0927f7301c0cc997deb6e4d30a6f50330
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-13 22:16:02 -0700

    Review of functions
    Messed up my function

--- iribe_ismael_assignment2/iribe_ismael_assignment2.js
+++ iribe_ismael_assignment2/iribe_ismael_assignment2.js
@@ -1,19 +1,20 @@
 //Ismael Iribe
+//MDV 1408
+//12Aug2014
+
+
+
 
 var kidNames = [ "Jack", "Ilene", "Kaleb", "Ingrid"];
-    agePerkid = [ 2, 7, 5, 10];
-    
-var cleanOneRoom = function(){
-    
-}
-for (var kidNumber = 0; toyNumber < kidNumber.length; kidNumber++) {
-    var kidNames = kidNames[kidNames],
-        toysKidOwns = kidBirthdaysPresents[kidNumber];
-        console.log("Need to pick up " + kidNames + " for "
-                    + toysKidOwns + " toys.");
-        for (var toys = 0; toys < toysKidOwns; toys += 1) {
+    agePerkid = [ 2, 7, 5, 10];//var cleanOneRoom = function(){
+for (var kidNumber = 0; kidNumber < kidNames.length; kidNumber++) {
+    var kidName = kidNames[kidNumber],
+        toysKidOwns = agePerkid[kidNumber];
+        console.log("Need to pick up " + kidName + " for "
+            + toysKidOwns + " toys.");
+    for (var toys = 0; toys < toysKidOwns; toys += 1) {
        var toysLeft = toysKidOwns - toys;
-       console.log (toys + " picked up," + toysLeft +" to finish.")
+       console.log (toys + " picked up," + toysLeft +" to finish.");
 }
-    console.log ("I'm done pickiing up after " + kidNames);
-}// for toyNumber
+    console.log ("I'm done pickiing up after " + kidName);
+};// for toyNumber
