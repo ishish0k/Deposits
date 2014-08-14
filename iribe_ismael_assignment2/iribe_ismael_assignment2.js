cc1809476a0835c12a0b6d5833b3965e98547ed3
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-13 21:45:35 -0700

    Beginning project
    I started late in the day because my kids keep making a mess.

--- iribe_ismael_assignment2/iribe_ismael_assignment2.js
+++ iribe_ismael_assignment2/iribe_ismael_assignment2.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+//Ismael Iribe
+
+var kidNames = [ "Jack", "Ilene", "Kaleb", "Ingrid"];
+    agePerkid = [ 2, 7, 5, 10];
+    
+var cleanOneRoom = function(){
+    
+}
+for (var kidNumber = 0; toyNumber < kidNumber.length; kidNumber++) {
+    var kidNames = kidNames[kidNames],
+        toysKidOwns = kidBirthdaysPresents[kidNumber];
+        console.log("Need to pick up " + kidNames + " for "
+                    + toysKidOwns + " toys.");
+        for (var toys = 0; toys < toysKidOwns; toys += 1) {
+       var toysLeft = toysKidOwns - toys;
+       console.log (toys + " picked up," + toysLeft +" to finish.")
+}
+    console.log ("I'm done pickiing up after " + kidNames);
+}// for toyNumber
