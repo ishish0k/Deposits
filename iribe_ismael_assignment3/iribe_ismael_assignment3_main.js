ba3bffc62154adaad43547696a1104ffd2698502
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-20 22:56:26 -0700

    Messing around with story
    About to call this quits but messing with arrays to create more
    console.log story

--- iribe_ismael_assignment3/iribe_ismael_assignment3_main.js
+++ iribe_ismael_assignment3/iribe_ismael_assignment3_main.js
@@ -1,45 +1,121 @@
 //Ismael Iribe
 //MDV 1408
 //21Aug2014
+
+
+
 var insectLegs = prompt("How many legs do bugs have? Pick between 1 and 8:", "");
 insectLegs = parseInt (insectLegs);
 
 switch (insectLegs) {
     case 1:
-        prompt("I don't think theres anything with one leg. You must think a worm is a bug too!")
+        console.log("I don't think theres anything with one leg. You must think a worm is a bug too!");
         break;
 
     case 2:
-            prompt("Water beetles will have special legs adapted for swimming, that makes them look like they have 2 legs. ")
+            console.log("Water beetles will have special legs adapted for swimming, that makes them look like they have 2 legs. ");
         break;
     case 3:
-            prompt("They do have 3 sets but how many total? ")
+            console.log("They do have 3 sets but how many total? ");
         break;
     case 4:
-            prompt("Did you read that in the Bible? ")
+            console.log("Did you read that in the Bible? ");
         break;
     case 5:
-            prompt("Your so close! ")
+            console.log("Your so close! ");
         break;
     case 6:
-            prompt("You know your insects. You must like bug collecting.")
+            console.log("You know your insects. You must like bug collecting.");
         break;
     case 7:
-            prompt("It might have been a spider that lost a leg. ")
+            console.log("It might have been a spider that lost a leg. ");
         break;
     case 8:
-            prompt("You must live near the desert to know a scorpion.")
+            console.log("You must live near the desert to know about scorpiones and spiders.");
     
+    default:
+        console.log("You did not enter a number between 1 and 8.");
+        break;  
+}
+
+var critterTypes = ["Bugs", "Arachnids", "Arthropods"];
+
+console.log(critterTypes);
+
+critterTypes[0] = "Bugs";
+
+console.log(critterTypes);
+critterTypes[3] = "Crustaceans";
+critterTypes[4] = "Rats";
+console.log(critterTypes);
+
+console.log("The length of the array "+ critterTypes.length);
+console.log(critterTypes);
+
+critterTypes[critterTypes.length]= "Jane";
+console.log(critterTypes);
+console.log("You have  " + critterTypes.length + " under your bed.");
+
+
+critterTypes.unshift("beetles");//unshift to the front, push to the back
+console.log(critterTypes);
+
+var lastCritter = critterTypes.pop();
+
+console.log ("You first collected " + lastCritter);
+console.log(critterTypes);
+
+var firstCritter = critterTypes.shift();
+
+console.log ("You must like to collect " + firstCritter);
+console.log(critterTypes);
+
+for (var i = 0; i< critterTypes.length; i++) {
+    if (critterTypes[i] === "rat"){
+        
+       console.log(critterTypes[i]+ " This is not a insect it's a mammal."); 
+        
+    } else{
+        
+        console.log(critterTypes[i]+" At least they are in the same family of arthropods.")
+        
+    };
     
+    //console.log("Index " + i + ": " + critterTypes[i]);
     
     
 }
+
+
+
 var handleData = function (json) {
     for (var i = 0; i < json.bugs.length; i++){
         var bugs = json.bugs[i];
-        console.log("Insect ID: " + bugs.insectid + ", Name: " + bugs.name + ", size: " + bugs.size + ", legs: " + bugs.legs);
+        console.log("It's just a " + bugs.name + "and it is  " + bugs.size + "centimiters, has " + bugs.legs + " legs.");
+    if (bugs.legs >=0 && bugs.legs <=5)
+    {
+        console.log("This is not a bug!");
+    }
+    if ( !(bugs.legs >= 0 && bugs.legs <=5))
+    {
+     console.log("Might be a bug or arachnid.");  
+    }
+    if (bugs.legs === 6 )
+    {
+     console.log("This is a bug.");  
+    }
+    if (bugs.legs === 8 )
+    {
+     console.log("This is in the arachnid family.");  
+    } 
+    if (bugs.legs === 7)
+    {
+        console.log("Must be that spider that lost his peg leg.");
+    }
     };
 };
 
 handleData (json2);
+
+
 
