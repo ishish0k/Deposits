d5823ba5621bf37aef3825ad651930a1deedb009
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-24 10:31:53 -0700

    Starting Project 4

--- iribe_ismael_assignment2/iribe_ismael_assignment2.js
+++ iribe_ismael_assignment2/iribe_ismael_assignment2.js
@@ -1,7 +1,8 @@
 //Ismael Iribe
 //MDV 1408
 //12Aug2014
-
+var say = function(message) { say(message); };// shorten say
+var say = function(message) { say(message); };// shorten say
 var decision = "Clean House"; // Boolean Variable
 var myConfirmation = true;
 var myPrompt;
@@ -9,24 +10,24 @@ var p = true; q = false, r = true;
 
 myPrompt = prompt("Do you like having a clean or messy home?")
     if (myPrompt === "clean") {
-        console.log("It is hard keeping up with these messy kids.")
+        say("It is hard keeping up with these messy kids.")
     } else {
-        console.log("How can you live like that?!")
+        say("How can you live like that?!")
     };
 
 if ((p && r) || q) {
-    console.log ("No matter how hard you try your home is always messy with kids.")
+    say ("No matter how hard you try your home is always messy with kids.")
 } else {
-    console.log("I think your home is very clean for now.")
+    say("I think your home is very clean for now.")
 };
 
 var toysInRoom = function(kidNames) {
     var toysRoom = [ "Bat", "Cars", "Dolls", "and blocks"];
     myConfirmation = confirm("Your kids left this in " + toysRoom + ".");
     if (myConfirmation === true) {
-        console.log ("Looks like we still need to clean up.");
+        say ("Looks like we still need to clean up.");
     } else {
-        console.log ("Ok lets keep on cleaning.");
+        say ("Ok lets keep on cleaning.");
     };
     return kidNames;
 
@@ -34,35 +35,33 @@ var toysInRoom = function(kidNames) {
 var numOfKids = 4;// iterator, counter
 
 while (numOfKids > 0) {
-    console.log(numOfKids + " Rooms to clean after.");
+    say(numOfKids + " Rooms to clean after.");
     numOfKids--;
     
 };/* will continue if you are using nothing
     * that is variable in your condition*/
-    console.log("No more rooms to clean. Take a break.")
+    say("No more rooms to clean. Take a break.")
 
 // for-loop
 for (var numOfKids = 4; numOfKids > 0; numOfKids--){
-       console.log(numOfKids + " rooms to clean.");
+       say(numOfKids + " rooms to clean.");
     
  };
  
-    console.log("You finally cleaned this house.")
+    say("You finally cleaned this house.")
 
- 
-var cleanAllRooms = function(kidNumber) {}
 var kidNames = [ "Jack", "Ilene", "Kaleb", "Ingrid"];
     agePerkid = [ 2, 7, 5, 10];//
     var cleanOneroom = function(){//scope where caraibles can or can't be used
         var kidName = kidNames[kidNumber],
         toysKidOwns = agePerkid[kidNumber];
-        console.log("You need to pick up " + kidName + "'s "
+        say("You need to pick up " + kidName + "'s "
             + toysKidOwns + " toys.");
     for (var toys = 0; toys < toysKidOwns; toys += 1) {
        var toysLeft = toysKidOwns - toys;
-       console.log (toys + " picked up," + toysLeft +" to finish.");
+       say (toys + " picked up," + toysLeft +" to finish.");
     };
-    console.log ("I'm done picking up after " + kidName);//argument
+    say ("I'm done picking up after " + kidName);//argument
     
 };
 cleanAllRooms();
@@ -74,16 +73,16 @@ for (var kidNumber = 0; kidNumber < kidNames.length; kidNumber++) {
 //encapsulation important to know
 myConfirmation = confirm("Do you think the house is clean now?");
     if (myConfirmation === true) {
-        console.log("I think it's because " + kidNames + " are asleep.");
+        say("I think it's because " + kidNames + " are asleep.");
     } else {
-        console.log("Oh man the kids might still be awake!");
+        say("Oh man the kids might still be awake!");
 };
 // Return Values
 //var agePerkid = function(toysInRoom) {
 //   var toys = 4,
 //   kidsMessLeft;
 //   if (toysInRoom<numOfKids) {
-//      console.log("You have to clean for a while.");
+//      say("You have to clean for a while.");
 //       return; //   }
 //    kidsMessLeft = Math.floor(toysInRoom/numOfKids);
 //   return kidsMessLeft;
