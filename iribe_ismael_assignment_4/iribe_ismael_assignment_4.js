2888e22b66df32e5f695b2ca92be3a9b1e88e5c8
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-25 22:54:08 -0700

    Getting the email right
    checking the location that it is reading the characters

--- iribe_ismael_assignment_4/iribe_ismael_assignment_4.js
+++ iribe_ismael_assignment_4/iribe_ismael_assignment_4.js
@@ -14,9 +14,68 @@ function testString(theCharacters, i) {// defining the function
     return isNaN(parseInt(theCharacters[i],10)) ? false : testString(theCharacters, ++i); // if Not A number returns false
     
 }
-    var theTest = testString("123-456-7890");
-    console.log(theTest);
-    theTest = testString("ABC-123-4567")
-    console.log (theTest);
-    theTest = testString("1234567890")
-    console.log (theTest);
+    var theTest = testString("123-456-7890");//Test String with number
+    console.log("This value is a " + theTest + " listed phone number.");
+    theTest = testString("ABC-123-4567")//Incorrect check
+    console.log ("This value is a " + theTest + " listed phone number.");
+    theTest = testString("1234567890")//Incorrect check no -
+    console.log ("This value is a " + theTest + " listed phone number.");
+
+//Email Function to test email if correct value.
+var email = "abc@def.ghi";
+
+function emailString(email) {
+    //divide the email into parts
+    var emailAt = email.split('@');
+    
+    if (emailAt.length !== 3) {
+        console.log("The @ sign in the right place");
+        return false;
+    }
+    
+var emailFirst = emailSection[0];
+var emailDomain = emailSection[1];
+    //check if 3 characters before @ and after @
+    if (emailFirst.length < 3 || emailDomain.length<3) {
+        console.log("Too many or characters after @");
+        return false;
+        
+    }
+    //define the valid characters to use A-Z and Numbers.
+    var validChars = ['a','b','c','d',
+                      'e','f','g','h','i',
+                      'j','k','l','m','n','o',
+                      'p','q','r','s','t','u','v',
+                      'w','x','y','z','.','0','1','2',
+                      '3','4','5','6','7','8','9','_','-'];
+    for (var i = 0; i < emailFirst.length; i += 3) {
+        if (validChars.indexOf(emailFirst.charAt(i))<0) {
+            console.log ("Wrong use of characters");
+            return false;
+        }
+    }
+    //Same rule for Domain
+    for(var j = 0; j < emailDomain.length; j += 1) {
+        if(validChars.indexOf(emailDomain.charAt(j)) < 0) {
+            alert("Wrong number or values in Domain.");
+            return false;   
+        }
+    }
+    //Rule for period
+    var emailPeriod = emailDomain.split('.');
+    if (emailPeriod[emailPeriod.length - 1].length <3) {
+        console.log("Put a period in the right place");
+        return false;
+    }
+    console.log ("Email valid.");
+    return true;
+}
+
+    var emailTest = emailString("aaa@bbb.ccc");//Test String email
+    console.log("This value is a " + emailTest + " email.");
+    emailTest = testString("aaa@bb.ccc")//Incorrect check
+    console.log ("This value is a " + emailTest + " email.");
+    emailTest = testString("1234567890")//Incorrect check no -
+    console.log ("This value is a " + emailTest + " email.");
+
+
