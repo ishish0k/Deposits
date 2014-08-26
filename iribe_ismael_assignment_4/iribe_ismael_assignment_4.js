662f255fa9253cba8f8217c9552f406cca53f70c
ishish0k <ishish0k@Ismaels-MacBook-Pro.local>
2014-08-25 21:26:34 -0700

    Changing the function to test number
    After looking at video for return values created function to test if
    true or false

--- iribe_ismael_assignment_4/iribe_ismael_assignment_4.js
+++ iribe_ismael_assignment_4/iribe_ismael_assignment_4.js
@@ -4,28 +4,19 @@
 //24Aug14
 
 //String Phone Number Pattern//
-var testString = function(numStr) {
-    if (numStr) {
-        //var hasParen = testString.substring(0,1) would check for parenthasis if there was in number
-        var areaCode = numStr.substring(0,3);//checks first 3 characters
-        var firstDash = numStr.substring (3,4);//checks 4th character
-        var firstThreeNum = numStr.substring (4,7);//checking next three characters
-        var  secondDash = numStr.substring (7,8);//checks for the ninth character
-        var lastFourNum = numStr.substring (8,12)//checks last four numbers
-    if (isNaN(areaCode,
-    "phone number - area code"))
-        ||(firstDash !="-")
-    ||(isNaN(firstThreeNum
-    "First Three Number's"))
-        ||(secondDash !="-")
-        ||(isANaN(lastFourNum))){
-        console.log ("This is a phone number");
-    }else{
-        console.log("This is a phone Number!");
+function testString(theCharacters, i) {// defining the function
+    i = i || 0;// defining interger
+    if (theCharacters.length !=12) return false;//if it doesn't equal length of 12 characters
+    if(i == theCharacters.length) return true;// if the letters lenght is the length
+    if(i == 3 || i == 7){ //for the first number of intergers throught the last
+        return theCharacters[i] == "-" ? testString(theCharacters, ++i) : false;// if it has the character - i or else it is false 
     }
-    }
-    response = "Hey" + numStr + "is something.";
-    return response;
-    };
+    return isNaN(parseInt(theCharacters[i],10)) ? false : testString(theCharacters, ++i); // if Not A number returns false
+    
+}
     var theTest = testString("123-456-7890");
+    console.log(theTest);
+    theTest = testString("ABC-123-4567")
+    console.log (theTest);
+    theTest = testString("1234567890")
     console.log (theTest);
